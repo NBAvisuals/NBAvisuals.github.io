5de17b8df2d3efb035921a664c383ff94d514a52
jensbob <jb.jens.jb@gmail.com>
2015-05-11 08:25:15 +0200

    g analytics test

--- catpages.sh
+++ catpages.sh
@@ -18,7 +18,16 @@ echo "$1"
 shift
 echo " </title>"
 echo "<style>*{background-color:#EEE;margin:0;}.Output{background-color:#FFF;margin: 1em auto;width:1000px;padding: 0 1em;}</style>"
-echo "</head><body>"
+echo "</head>"
+echo "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62818047-1', 'auto');
+  ga('send', 'pageview');
+
+</script><body>"
 
 awk 'BEGIN{body=0; count=0}{
 if($1=="<p"){body=1; count+=1;}
